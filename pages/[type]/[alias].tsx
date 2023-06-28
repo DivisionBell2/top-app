ea4187427158ba5df2bb875e86789ade36e0ebf3
division_bell2 <neglavmag@mail.ru>
2023-06-28 15:45:03 +0300

    added yandex metrika

--- pages/[type]/[alias].tsx
+++ pages/[type]/[alias].tsx
@@ -8,14 +8,24 @@ import { ProductModel } from "@/interfaces/product.interface";
 import { firstLevelMenu } from "@/helpers/helpers";
 import { TopPageComponent } from "@/page-components";
 import { API } from "@/helpers/api";
+import Head from 'next/head';
 
 function TopPage({ firstCategory, page, products }: TopPageProps): JSX.Element {
   return (
-    <TopPageComponent
-    firstCategory={firstCategory}
-    page={page}
-    products={products}
-    />
+    <>
+    <Head>
+      <title>{page.metaTitle}</title>
+      <meta name="description" content={page.metaDescription} />
+      <meta property="og:title" content={page.metaTitle} />
+      <meta property="og:description" content={page.metaDescription} />
+      <meta property="og:type" content="article" />
+    </Head>
+      <TopPageComponent
+        firstCategory={firstCategory}
+        page={page}
+        products={products}
+      />
+    </>
   )
 }
 
@@ -55,12 +65,12 @@ export const getStaticProps: GetStaticProps<TopPageProps> = async ({ params }: G
     const { data: menu } = await axios.post<MenuItem[]>(API.topPage.find, {
       firstCategory: firstCategoryItem.id
     });
-  
+
     if (menu.length == 0) {
       return {
         notFound: true
       }
-    } 
+    }
     const { data: page } = await axios.get<TopPageModel>(API.topPage.byAlias + params.alias);
     const { data: products } = await axios.post<ProductModel[]>(API.product.find, {
       category: page.category,
@@ -74,7 +84,7 @@ export const getStaticProps: GetStaticProps<TopPageProps> = async ({ params }: G
         products
       }
     }
-  } catch(e) {
+  } catch (e) {
     return {
       notFound: true
     }
